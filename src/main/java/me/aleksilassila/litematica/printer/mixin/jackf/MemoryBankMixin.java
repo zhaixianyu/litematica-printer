4db8b7726cbf750a06516123b214bc82cd1bd092
zhaixianyu <1141927681@qq.com>
2023-12-16 19:22:48 +0800

    20.2

--- src/main/java/me/aleksilassila/litematica/printer/mixin/jackf/MemoryBankMixin.java
+++ src/main/java/me/aleksilassila/litematica/printer/mixin/jackf/MemoryBankMixin.java
@@ -3,9 +3,12 @@ package me.aleksilassila.litematica.printer.mixin.jackf;
 import carpet.script.language.Sys;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
@@ -31,10 +34,20 @@ public abstract class MemoryBankMixin {
     @Unique
     boolean search = true;
     @Unique
-    public void search(Identifier key){
+    public void search(Identifier key1){
         search = false;
         MinecraftClient client = MinecraftClient.getInstance();
-
+        memories.keySet().forEach(key -> {
+            if(key1.equals(key) && MemoryBank.INSTANCE != null) {
+                List<SearchResult> positions = MemoryBank.INSTANCE.getPositions(key, new SearchRequest());
+                RegistryKeys.WORLD.getRegistry();
+                if (client.world != null) {
+                    RegistryKey<World> dimensionKey = client.world.getRegistryKey();
+                }
+                RegistryKey<World> key2 = RegistryKey.of(RegistryKeys.WORLD, key);
+            }
+        });
+        search = true;
     }
     @Inject(at = @At("RETURN"),method = "getPositions")
     private void getPositions(Identifier key, SearchRequest request, CallbackInfoReturnable<List<SearchResult>> cir){
@@ -42,5 +55,8 @@ public abstract class MemoryBankMixin {
         memories.keySet().forEach(e -> System.out.println(e.toString()));
 
         List<SearchResult> returnValue = cir.getReturnValue();
+        if (returnValue.isEmpty() && search) {
+            search(key);
+        }
     }
 }
